f68c13f7bf4764d2749c7f46198cb8f109528859
Brian Henry <94brianhenry@gmail.com>
2018-05-22 08:30:09 -0700

    update some files

--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
 import './styles.css';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import $ from 'jquery';
 import { vowelCount } from './journal';
 import { consonantsCount } from './journal';
 
